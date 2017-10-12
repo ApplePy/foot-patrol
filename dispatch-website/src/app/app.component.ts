f024e146fbd44997eb985f72d9390fe84426fc52
rcollier <rcollier@murrayweb.ca>
2017-10-12 17:29:45 -0400

    Display requests in list. rough

--- dispatch-website/src/app/app.component.ts
+++ dispatch-website/src/app/app.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Foot-Patrol Dispatcher Website';
+  
+  requests: Request[]=[
+    {id:1, name: 'test1', location: '345 254', time: 1130},
+    {id:2, name: 'test2', location: '1233 456', time: 230},
+    {id:3, name: 'test3', location: '789 101112', time: 1700}
+  ];
 }
 export class Request {
   id: number;
@@ -14,3 +20,6 @@ export class Request {
   location: string; //change this to the correct type when we determine what that is
   time: number; //change this to the correct type. probably a time type lying around somewhere
 }
+
+
+
