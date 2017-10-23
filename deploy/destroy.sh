290b915ba17590bf60efd3bedb830aebd3b7d778
Darryl Murray <darrylmurray@me.com>
2017-10-22 21:07:41 -0400

    Added scripts for deployment to kubernetes.

---
+++ deploy/destroy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+D_ENV=${1}
+TAG=${2}
+URL=${3}
+export DEPLOY_ENVIRONMENT=${D_ENV}
+export DEPLOY_TAG=${TAG}
+export DEPLOY_URL=${URL}
+for f in ./deploy/*-deploy.yml
+do
+  envsubst < $f > "./deploy/.generated/$(basename $f)"
+done
+
+kubectl delete -f ./deploy/.generated/
