535a26b2b39f55e583b1a4560ebf5fe0fcf7a35d
mromao <mromao@uwo.ca>
2017-11-01 01:45:29 -0400

    Fixed the Adapter to Enable Tabular View + Modified UI a little more

---
+++ FootPatrol/Droid/FootPatrolAdapter.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FootPatrol.Droid
+{
+    [Activity(Label = "FootPatrolAdapter")]
+    public class FootPatrolAdapter : FragmentPagerAdapter
+    {
+        public FootPatrolAdapter(Android.Support.V4.App.FragmentManager fm) : base(fm)
+        {
+
+        }
+
+        public override int Count {
+            get { return 1; }
+        } 
+
+        public override Android.Support.V4.App.Fragment GetItem(int position)
+        {
+            return (Android.Support.V4.App.Fragment) PickUpActivity.newInstance();
+        }
+
+    }
+}
