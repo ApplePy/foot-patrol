abef2d81bc287c16c231046f5e10d148e78b8a47
rcollier <rcollier@murrayweb.ca>
2017-11-13 11:05:02 -0500

    removed test code

--- src/dispatch-website/src/app/ftp-request.service.ts
+++ src/dispatch-website/src/app/ftp-request.service.ts
@@ -13,25 +13,19 @@ export class FtpRequestService {
 
   constructor(private http: Http) { }
 
-  Requests1: Request[]=[
-    {id:1, name: 'test1',to_Location: "SEB",additional_info:"null",archived:false, timestamp: "2017-10-25T07:32:19.000Z",from_Location:"UCC"},
-    {id:2, name: 'test2',to_Location: "SEB",additional_info:"null",archived:false, timestamp: "2017-10-26T07:13:52.000Z",from_Location:"UCC"},
-  ]
-
 
   getRequests(): Promise<Request[]> {
-    return Promise.resolve(this.Requests1);
     //get first 10 requests
-    /*return this.http.get(this.requestURL + "?offset=0&count=10")
+    return this.http.get(this.requestURL + "?offset=0&count=10")
             .toPromise()
             .then(response => response.json().requests as Request[])
             .catch(this.handleError);
-  */}
+  }
    
   archiveRequest(request){
     var patchURL=this.requestURL + "/"+request.id;
     this.http.patch(patchURL,{
-      params: new URLSearchParams().set('archived',request.archived)
+      params: new HttpParams().set('archived',request.archived)
     }
   )
   }
