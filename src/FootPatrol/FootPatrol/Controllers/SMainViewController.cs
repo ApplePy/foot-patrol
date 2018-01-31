01c8feed05245db5f484a27c3385e5346318c6c6
Kian Paliani <kpaliani@uwo.ca>
2018-01-31 15:32:04 -0500

    Fixed up code review issues.  Refactored out code.  Added in a mutex.

--- src/FootPatrol/FootPatrol/Controllers/SMainViewController.cs
+++ src/FootPatrol/FootPatrol/Controllers/SMainViewController.cs
@@ -1,92 +1,99 @@
 ﻿using System;
 using System.Threading.Tasks;
-//using Foundation;
-//using UIKit;
+using System.Threading;
 namespace FootPatrol
 {
     public class SMainViewController
     {
         public int requestID;
         public bool requestSent = false;
+        public Mutex mutex;
 
         private IMainViewController inst;
 
         public SMainViewController(IMainViewController instance)
         {
             inst = instance;
+            mutex = new Mutex();
         }
 
-        public async void RequestButtonClicked (string name, string fromLocation, string toLocation, string additionalInfo) {
+        //Send request
+        public async void SendRequest(string name, string fromLocation, string toLocation, string additionalInfo) 
+        {
+            //Send request
+            try
+            {
+                //Send request
+                requestID = await RequestService.SendFootPatrolRequest(name, fromLocation, toLocation, additionalInfo);
 
+                //Change button to cancel
+                inst.DisplayPopup("Request Sent", "Your SafeWalk request has been sent.");
+                inst.SetRequestButtonTitle("Cancel SafeWalk Request");
+                requestSent = true;
+            }
+            catch
+            {
+                //Display error
+                inst.DisplayPopup("Error", "Please check your input.");
+            }
+        }
+
+        //Cancel request
+        public async void CancelRequest() 
+        {
+            //Cancel footpatrol request
+            try
+            {
+                //Cancel request
+                await RequestService.DeleteFootPatrolRequest(requestID);
+
+                //Change button to request
+                inst.DisplayPopup("Request Cancelled", "Your SafeWalk request has been cancelled.");
+                inst.SetRequestButtonTitle("Request SafeWalk");
+                requestSent = false;
+            }
+            catch
+            {
+                //Popup with error
+                inst.DisplayPopup("Error", "There was an error cancelling request.");
+            }
+        }
+
+        //Request button clicked
+        public void RequestButtonClicked (string name, string fromLocation, string toLocation, string additionalInfo) {
+
+            //Lock mutex and disable button
+            mutex.WaitOne();
+            inst.SetRequestButtonEnabled(false);
+
+            //If button is set to request
             if (requestSent == false)
             {
-
-                requestSent = true;
-                inst.SetRequestButtonEnabled(false);
-
                 //Check if all required input fields have been filled out
                 if (String.IsNullOrEmpty(name) || String.IsNullOrEmpty(fromLocation) || String.IsNullOrEmpty(toLocation))
                 {
-                    inst.DisplayPopup("Error", "Please check your input.");
-                    return;
-                }
-
-                //Send footpatrol request
-                try
-                {
-                    requestID = await RequestService.SendFootPatrolRequest(name, fromLocation, toLocation, additionalInfo);
-                }
-                catch (Exception error)
-                {
-                    requestSent = false;
-
+                    //Display error
                     inst.DisplayPopup("Error", "Please check your input.");
 
+                    //Enable button and release mutex
                     inst.SetRequestButtonEnabled(true);
-
+                    mutex.ReleaseMutex();
                     return;
                 }
 
-                inst.SetRequestButtonTitle("Cancel SafeWalk Request");
-
-                inst.DisplayPopup("Request Sent", "Your SafeWalk request has been sent.");
-
-                inst.SetRequestButtonEnabled(true);
+                SendRequest(name, fromLocation, toLocation, additionalInfo);
 
             }
-            else
+            else //If button is set to cancel
             {
-
-                requestSent = false;
-                inst.SetRequestButtonEnabled(false);
-
-                //Cancel footpatrol request
-                try
-                {
-                    await RequestService.DeleteFootPatrolRequest(requestID);
-                }
-                catch (Exception error)
-                {
-                    requestSent = true;
-
-                    //Popup with error
-                    inst.DisplayPopup("Error", "There was an error cancelling request.");
-
-                    inst.SetRequestButtonEnabled(true);
-
-                    return;
-                }
-
-                inst.SetRequestButtonTitle("Request SafeWalk");
-
-                //Popup saying that request has been sent
-                inst.DisplayPopup("Request Cancelled", "Your SafeWalk request has been cancelled.");
-
-                inst.SetRequestButtonEnabled(true);
-
+                CancelRequest();
             }
-        }
 
+            //Enable button and unlock mutex
+            inst.SetRequestButtonEnabled(true);
+            mutex.ReleaseMutex();
+
+        }
     }
 }
 
