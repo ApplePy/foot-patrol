9763a76507c879ae8c99039f317a8f360e34f0b1
Darryl Murray <darrylmurray@me.com>
2018-02-28 21:58:45 -0500

    Created volunteer pairing backend.

---
+++ src/backend/src/services/sql-pairings-manager.ts
@@ -0,0 +1,128 @@
+import { inject, injectable } from "inversify";
+import { IFACES } from "../ids";
+import { IVolunteerPairingManager } from "../interfaces/ipairing-manager";
+import { ISQLService } from "../interfaces/isql-service";
+import { IVolunteersManager } from "../interfaces/ivolunteers-manager";
+import { Volunteer } from "../models/volunteer";
+import { VolunteerPairing } from "../models/volunteer-pairing";
+import { SQLAbstractManager } from "./sql-abstract-manager";
+
+/**
+ * Data manager for handling retrieving requested data from multiple possible persistence layers.
+ *
+ * This class expects inputs to have already been sanitized and/or limited.
+ */
+@injectable()
+export class SQLVolunteerPairingManager extends SQLAbstractManager implements IVolunteerPairingManager {
+
+  private db: ISQLService;
+  private volmgr: IVolunteersManager;
+
+  /**
+   * Constructor.
+   *
+   * @param db The SQL database implementation to use.
+   */
+  constructor(
+    @inject(IFACES.ISQLSERVICE) db: ISQLService,
+    @inject(IFACES.IVOLUNTEERSMANAGER) volmgr: IVolunteersManager
+  ) {
+    super();
+
+    // Save db and volunteers manager
+    this.db = db;
+    this.volmgr = volmgr;
+  }
+
+  /**
+   * Get a volunteer pairing from the backend.
+   *
+   * @param id The id of the pairing to get
+   */
+  public getPairing(id: number) {
+    // Make DB request
+    return this.db.makeQuery("SELECT * FROM `volunteer_pairing` WHERE id=?", [id])
+    // Ensure data was returned
+    .then((request) => (request.length > 0) ? request[0] : Promise.reject(new Error("Not Found")))
+    // Convert to appropriate format and return
+    .then((val) => this.reconstructVolunteerPairing(val));
+  }
+
+  /**
+   * Get a list of volunteer pairs from the backend.
+   *
+   * Defaults to returning inactive pairs.
+   *
+   * @param filter Dictionary to be plugged into the SQL `WHERE` parameter as "AND"
+   */
+  public getPairings(filter?: Map<string, any>) {
+    // Get filter array
+    const filterArray = (filter !== undefined) ? filter.values() : [];
+    const questionMarks = this.generateQuestionMarks((filter !== undefined) ? filter.keys() : [], " AND ");
+
+    // Query for data
+    return this.db.makeQuery(
+      `SELECT * FROM \`volunteer_pairing\`${(questionMarks.length > 0) ? " WHERE " : ""}${questionMarks}`,
+      [...filterArray])
+    .then((requests) => {
+      // Resolve all the volunteer objects
+      const resolves = requests.map((x: any) => this.reconstructVolunteerPairing(x)) as [Promise<VolunteerPairing>];
+
+      // Return when all objects are resolved
+      return Promise.all(resolves);
+    });
+  }
+
+  /**
+   * Create a new volunteer pairing. Returns new ID.
+   *
+   * @param pairing The new pairing to store.
+   */
+  public createPairing(pairing: VolunteerPairing) {
+    // Sanity check
+    if (!pairing.Valid()) {
+      return Promise.reject("Requesting save of invalid Pairing");
+    }
+
+    return this.db.makeQuery(
+      "INSERT INTO `volunteer_pairing` (volunteer_one, volunteer_two, active) VALUES(?,?,?)",
+      [
+        pairing.volunteer_one.id,
+        pairing.volunteer_two.id,
+        pairing.active
+      ])
+    .then((results: any) => results.insertId as number);
+  }
+
+  // NOTE: Not supporting deleting a volunteer pairing due to references from other tables.
+
+  /**
+   * Update a volunteer pairing with new active status.
+   *
+   * @param pairing The volunteer pairing to be updated.
+   */
+  public toggleActive(pairing: VolunteerPairing) {
+    return this.db.makeQuery(`UPDATE \`volunteer_pairing\` SET active=? WHERE id=?`,
+    [Number(pairing.active), pairing.id])
+    .then((result) => (result.affectedRows > 0) ?
+                      Promise.resolve() :
+                      Promise.reject(new Error("Not Found")));
+  }
+
+  /**
+   * Resolves the volunteer IDs from the SQL pairing record.
+   *
+   * @param obj The raw pairing record from the SQL DB.
+   */
+  private reconstructVolunteerPairing(obj: {[key: string]: any}) {
+    // Retrieve volunteers for this pairing
+    const volOnePromise = this.volmgr.getVolunteer(Number(obj.volunteer_one));
+    const volTwoPromise = this.volmgr.getVolunteer(Number(obj.volunteer_one));
+    return Promise.all([volOnePromise, volTwoPromise])
+    .then((volunteers) => new VolunteerPairing(
+        volunteers[0],
+        volunteers[1],
+        obj.active,
+        obj.id));
+  }
+}
