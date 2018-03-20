db1aaf94a4b35660fdb38cf671a0f88522bedc5e
rcollier <rcollier@murrayweb.ca>
2018-03-20 14:53:26 -0400

    created login page and wrote tests for it

--- src/dispatch-website/src/app/login-page/login-page.component.spec.ts
+++ src/dispatch-website/src/app/login-page/login-page.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms';
 
 import { LoginPageComponent } from './login-page.component';
 
@@ -8,6 +10,8 @@ describe('LoginPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule,
+      FormsModule],
       declarations: [ LoginPageComponent ]
     })
     .compileComponents();
@@ -21,5 +25,36 @@ describe('LoginPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('login', () => {
+    beforeEach(() => {
+      fixture = TestBed.createComponent(LoginPageComponent);
+      component = fixture.componentInstance;
+      spyOn(component.router, 'navigateByUrl');
+      spyOn(sessionStorage, 'setItem');
+      fixture.detectChanges();
+    });
+
+    it('should let in administrators', () => {
+      component.username = 'root';
+      component.password = 'root';
+      component.login();
+      expect(sessionStorage.setItem).toHaveBeenCalledWith('currentUser', 'Admin');
+      expect(component.router.navigateByUrl).toHaveBeenCalledWith('/request-list');
+    });
+    it('should let in dispatch users', () => {
+      component.username = component.users[0].username;
+      component.password = component.users[0].password;
+      component.login();
+      expect(sessionStorage.setItem).toHaveBeenCalledWith('currentUser', 'Dispatcher');
+      expect(component.router.navigateByUrl).toHaveBeenCalledWith('/request-list');
+    });
+    it('should block when given incorrect user information', () => {
+      component.username = 'foo';
+      component.password = 'bar';
+      component.login();
+      expect(component.errorMsg).toBe('Incorrect login information');
+    });
   });
 });
--- src/dispatch-website/src/app/login-page/login-page.component.ts
+++ src/dispatch-website/src/app/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  errorMsg: string;
+  users: User[];
+  username: string;
+  password: string;
+
+  constructor(public router: Router) { }
 
   ngOnInit() {
+    this.users = [{
+      username: 'Rowan',
+      password: 'Collier'
+    }, {
+      username: 'Darryl',
+      password: 'Murray'
+    }, {
+      username: 'Kian',
+      password: 'Paliani'
+    }, {
+      username: 'Michael',
+      password: 'Romao'
+    }];
   }
 
+  /**
+   * no backend support :(
+   * run shittyHack.exe
+   */
+  login() {
+    let found = false;
+    if (this.username === 'root' && this.password === 'root') {
+      sessionStorage.setItem('currentUser', 'Admin');
+      this.router.navigateByUrl('/request-list');
+    } else {
+      for (let i = 0; i < this.users.length; i++) {
+        if (this.users[i].username === this.username && this.users[i].password === this.password) {
+          sessionStorage.setItem('currentUser', 'Dispatcher');
+          found = true;
+          break;
+        }
+      }
+      if (found) {
+        this.errorMsg = '';
+        this.router.navigateByUrl('/request-list');
+      } else {
+        this.errorMsg = 'Incorrect login information';
+      }
+    }
+  }
+
+}
+
+class User {
+  username: string;
+  password: string;
 }
