d9539ea6a5c9cd155bbc09c75c7360e73da72e71
Darryl Murray <darrylmurray@me.com>
2018-03-23 15:54:32 -0400

    Update server.ts so localhost:4200 can access it (for Kian and Rowan)

--- src/backend/src/server.ts
+++ src/backend/src/server.ts
@@ -75,7 +75,7 @@ export class Server {
     // Control response headers
     this.app.use((request: express.Request, response: express.Response, next: express.NextFunction) => {
       // Leave the dual server system in place for everything except production
-      response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      //response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
       response.header("Access-Control-Allow-Methods", "POST, PATCH, GET, PUT, DELETE, OPTIONS");
       next();
     });
